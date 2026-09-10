0ccdab262d3701cec9bf82891f72b1c3f13c2d58
rupali-sanodiya <sanodiyav76@gmail.com>
2026-09-10 21:46:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (20.42%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverse(int x) {
+       long rev=0;
+       while(x!=0){
+        long digit=x%10;
+        rev=(rev*10)+digit;
+    
+       if(rev<INT_MIN || rev>INT_MAX){
+        return 0;
+       }
+         x/=10;}
+       return (int)rev;
+    }
+};
